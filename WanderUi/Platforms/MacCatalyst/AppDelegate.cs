dafd62a47bf19bf215da31abb02b130b31884af0
Jamie Lord <jamie@lord.technology>
2023-10-14 10:22:48 +0100

    New MAUI project

---
+++ WanderUi/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WanderUi
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
